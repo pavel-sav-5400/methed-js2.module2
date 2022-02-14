d8caeaa640de040e59498da64e7b3de0c93abd80
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-02-14 19:38:44 +1000

    написал функции для работы со списком

--- lesson_03/script.js
+++ lesson_03/script.js
@@ -1,14 +1,27 @@
-/* eslint-disable no-unused-vars */
 'use strict';
 
 const propsList = document.querySelector('.props__list');
+const delLastElem = () => propsList.lastChild.remove();
+const clearList = () => propsList.textContent = '';
+
+propsList.style.cssText = `
+font-size: 20px;
+color: darkgreen;
+`;
 
 const addValue = () => {
-  const useStr = prompt('Введите строку');
+  const useStr = prompt('Введите строку').trim();
   propsList.insertAdjacentHTML('beforeend', `<li class="list__item">
 ${useStr}</li>`);
-  if (useStr === null) {
-    return alert('!!!');
+  if (useStr === null || useStr === 'exit') {
+    return delLastElem();
+  }
+  if (useStr === 'clear') {
+    return clearList();
+  }
+  if (useStr === '') {
+    delLastElem();
+    return addValue();
   }
   return addValue();
 };
