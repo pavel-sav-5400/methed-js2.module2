7e7ae696df117b7e5849a85154688ccff4865dd2
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-02-14 10:35:53 +1000

    добавил функцию addval

--- lesson_03/script.js
+++ lesson_03/script.js
@@ -1,2 +1,15 @@
+/* eslint-disable no-unused-vars */
 'use strict';
 
+const propsList = document.querySelector('.props__list');
+
+const addValue = () => {
+  const useStr = prompt('Введите строку');
+  propsList.insertAdjacentHTML('beforeend', `<li class="list__item">
+${useStr}</li>`);
+  if (useStr === null) {
+    return alert('!!!');
+  }
+  return addValue();
+};
+addValue();
